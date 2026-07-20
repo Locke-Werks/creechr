c2073fb6e87160ce0d0d2347b999c2cb6bf14a1f
Joe Hancuff <jhancuff@hancuffmediagroup.com>
2026-07-20 11:15:04 -0600

    build.bat: vs path caught up with reality (2022 dir, not 18)

--- scripts/build.bat
+++ scripts/build.bat
@@ -7,7 +7,7 @@ rem if you don't have qt at C:\Qt\6.8.3, change the qt path below.
 rem if you don't have visual studio 2022/2026, change the vs path below.
 rem if you have neither, this is the wrong project for you, sorry.
 
-call "C:\Program Files\Microsoft Visual Studio\18\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 if errorlevel 1 (
     echo vcvars64.bat failed. is visual studio actually installed?
     exit /b 1
